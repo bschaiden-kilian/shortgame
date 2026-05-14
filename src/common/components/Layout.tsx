3a179a134c46e65f65066be7a66253ea7c0a23fc
Kilian Bschaiden <kilian.bschaiden@htlpinkafeld.at>
2026-05-14 17:38:27 +0200

    overflow fix v2

--- src/common/components/Layout.tsx
+++ src/common/components/Layout.tsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom'
 
 const Layout = () => {
     return (
-        <div className="h-screen bg-mist-950 text-white pt-20 py-10 px-5">
+        <div className="h-screen overflow-y-auto overflow-x-hidden bg-mist-950 text-white pt-20 py-10 px-5">
             <Outlet></Outlet>
         </div>
     )
